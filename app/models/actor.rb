c8328f7a0f26ceaebfcd9d08bc735c99e10f19e1
lmjcbs <hello@liamjacobs.dev>
2019-11-20 22:39:20 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    char = Character.find_by(actor_id: self.id)
+    show = Show.find_by(id: char.show_id)
+    "#{char.name} - #{show.name}"
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
   
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    list = []
+    char = Character.find_by('show_id = ?', self.id)
+    actor = Actor.find_by('id = ?', char.actor_id)
+    list << actor.full_name
+  end
+
+  def build_network(options)
+    self.network = Network.create(options)
+  end
+
 end
